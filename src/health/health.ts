afa6241e94997c13cb919ce36abfb672a2539cc2
Chinweike99 <innocentakwolu1@gmail.com>
2025-07-21 11:42:52 +0100

    fixings

--- src/health/health.ts
+++ src/health/health.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 
 export const HealthCheck =(req: Request, res: Response) => {
-    res.status(200).json({ message: 'Health Check Successful' });
+    res.status(200).json({ message: 'Health Check Successful ..✴️' });
 }
