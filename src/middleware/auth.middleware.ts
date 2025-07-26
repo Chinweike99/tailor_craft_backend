163e17a75fa099aa4f3f8f43ea3d1061ce70175a
Chinweike99 <innocentakwolu1@gmail.com>
2025-07-26 23:50:22 +0100

    index.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,3 +1,51 @@
+import { PrismaClient } from '@prisma/client'
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken'
-import configDb from '../config/config'
+import { UnauthorizedError } from '../utils/error.utils';
+import config from '../config/config';
+
+
+
+
+const prisma = new PrismaClient();
+
+export const authenticate = async(req:Request, res: Response, next: NextFunction) => {
+    const token = req.header("Authorization")?.replace("Bearer ", '');
+    if(!token) {
+        throw new UnauthorizedError("Authentication required");
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.jwt.secret) as {id: string};
+        const user = await prisma.user.findUnique({
+            where: {id: decoded.id},
+            select:{
+                id: true,
+                email: true,
+                role: true,
+                isVerified: true
+            }
+        });
+
+        if(!user) {
+            throw new UnauthorizedError("User not Found")
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new UnauthorizedError("Invalid token")
+    }
+}
+
+
+export const adminOnly = (req: Request, res: Response, next: NextFunction) => {
+    if(req.user?.role !== 'ADMIN') {
+        throw new UnauthorizedError('Admin access required')
+    };
+    next();
+}
+
+
+
+
 
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,34 @@
+import z from "zod";
+
+export type ZodSchemaType<T> = z.ZodType<T, any, any>;
+
+export interface ApiError {
+    message: string;
+    statusCode: number;
+    errors?: Record<string, string[]>;
+};
+
+export interface PaginationParams {
+    page?: number;
+    limit?: number;
+}
+
+export interface PaginationFilters extends PaginationParams {
+    [key: string]: any;
+}
+
+export interface UploadedFile {
+    filename: string;
+    originalname: string;
+    encoding: string;
+    mimetype: string;
+    size: number;
+    destination: string;
+    path: string;
+    buffer?: Buffer;
+}
+
+
 // User Types
 export interface User {
   id: string;
