0bfcffc3a0dcd3dd8d0dbad1c533436b7c9255a4
Chinweike99 <innocentakwolu1@gmail.com>
2025-07-26 05:07:44 +0100

    config.ts

--- src/config.ts/config.db.ts
+++ src/config.ts/config.db.ts
@@ -11,3 +11,49 @@ export const prisma = new PrismaClient({
         }
     }
 })
+
+const env = process.env.NODE_ENV || 'development';
+const port = process.env.port || 5000;
+
+export default {
+    env, port,
+    database: {
+        url: process.env.EXTERNAL_DB_URL || ""
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET as string,
+        accessExpiration: process.env.JWT_ACCESS_EXPIRATION as string,
+        refreshExpiration: process.env.JWT_REFRESH_EXPIRATION as string
+    },
+    redis: {
+        url: process.env.REDIS_URL
+    },
+    paystack: {
+        secretKey: process.env.PAYSTACK_SECRET_KEY,
+        publicKey: process.env.PAYSTACK_PUBLIC_KEY
+    },
+    cloudinary: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME as string,
+        apiKey: process.env.CLOUDINARY_API_KEY as string,
+        apiSecret: process.env.CLOUDINARY_API_SECRET as string,
+    },
+    email: {
+        smtp: {
+            host: process.env.SMTP_HOST as string,
+            port: Number(process.env.SMTP_PORT),
+            auth: {
+                user: process.env.SMTP_USERNAME as string,
+                pass: process.env.SMTP_PASSWORD as string
+            }
+        },
+        from: process.env.EMAIL_FROM as string
+    },
+    otp: {
+        length: 6,
+        expiresInMinutes: 5
+    },
+    admin: {
+        email: "chinweiketwitter@gmail.com",
+        name: "Chinweike AKwolu"
+    }
+}
