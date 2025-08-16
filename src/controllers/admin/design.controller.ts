db3bd195ec37d3b9df8b341d9b62c3bf75d2b47a
Chinweike99 <innocentakwolu1@gmail.com>
2025-08-16 15:12:37 +0100

    fix: lint fail

--- src/controllers/admin/design.controller.ts
+++ src/controllers/admin/design.controller.ts
@@ -1,30 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { createDesignSchema, getDesignsSchema, updateDesignSchema } from "../../validation/design";
-import { createDesign, deleteDesign, getDesignById, getDesigns, updateDesign, uploadDesignImages } from "../../services/admin/design.service";
+import { createDesign, deleteDesign, getDesignById, getDesigns, updateDesign } from "../../services/admin/design.service";
 
 
-
-// export const createDesignController = async(req: Request, res: Response, next: NextFunction) => {
-//     console.log("Endpoint is hit")
-//     try {
-//         const validateData  = createDesignSchema.parse(req.body);
-//         if(!req.files || !Array.isArray(req.files)){
-//             throw new Error("No images uploaded")
-//         }
-//         console.log("Endpoint is hit", validateData)
-//         const images = await uploadDesignImages(req.files)
-//         console.log("Images is hit", images)
-//         const design = await createDesign(validateData, images.map(img => img.secure_url));
-//         console.log("Design is hit " + JSON.stringify(design, null, 2));
-//         res.status(200).json({
-//             status: "success",
-//             design
-//         })
-//         console.log(design)
-//     } catch (error) {
-//         next(error)
-//     }
-// }
 
 export const createDesignController = async(req: Request, res: Response, next: NextFunction) => {
     console.log("Endpoint is hit")
