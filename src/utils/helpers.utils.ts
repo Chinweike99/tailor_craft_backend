f5e8f89633353a7c7306106ef372cbed6642432f
Chinweike99 <innocentakwolu1@gmail.com>
2026-01-07 03:05:36 -0500

    updated

--- src/utils/helpers.utils.ts
+++ src/utils/helpers.utils.ts
@@ -1,6 +1,5 @@
 import crypto from "crypto";
 import nodemailer from "nodemailer";
-import { Resend } from "resend";
 import config from "../config/config";
 import argon2 from "argon2";
 import { sendEmail as sendBrevoEmail } from "./email.service";
